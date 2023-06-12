3f2357edda10cadf9f07d7341022e934e9ca4ab1
Osztopáni Kristóf <kristofosztopani@gmail.com>
2023-06-13 01:13:37 +0200

    save project to git

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # mandelbrot
 Using cuda to generate the mandelbrot set and zoom in
+
+This is a project for a coursera curse
+
+You can run the compile the code with the following command:
+
+```bash 
+nvcc mandelbrot.cu -I. -o mandelbrot
+```
+
+And then run it and create a video:
+
+```bash
+./mandelbrot
+ffmpeg -framerate 30 -i frames/frame%d.png -c:v libx264 -pix_fmt yuv420p mandelbrot_video.mp4
+
+```
+
+The project uses the following libraries:
+stb_image_write:
+  - A single header only library to generate png images
+
+The project uses the following tools:
+ffmpeg:
+  - To generate the video from the images
+
+A sample video is provided in the repo
+you can see it here
+[mandelbrot video](mandelbrot_video.mp4)
---
+++ mandelbrot.cu
@@ -0,0 +1,92 @@
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include <cmath>
+#include <cstdint>
+#include <cuda_runtime.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+constexpr int WIDTH = 800;
+constexpr int HEIGHT = 600;
+constexpr int MAX_ITERATIONS = 1000;
+constexpr float ZOOM_FACTOR = 0.9;
+constexpr int FRAMES = 10 * 60;
+
+struct Color
+{
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+
+    __device__ Color(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b) {}
+};
+
+__device__ Color getColor(int iteration)
+{
+    int red = (iteration % 8) * 32;
+    int green = (iteration % 16) * 16;
+    int blue = (iteration % 32) * 8;
+
+    return Color(red, green, blue);
+}
+
+__global__ void generateMandelbrot(uint8_t* image, float zoom, float centerX, float centerY)
+{
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x >= WIDTH || y >= HEIGHT)
+        return;
+
+    float zx = 0.0;
+    float zy = 0.0;
+    float cx = centerX + (x - WIDTH / 2) / (0.5 * zoom * WIDTH);
+    float cy = centerY + (y - HEIGHT / 2) / (0.5 * zoom * HEIGHT);
+
+    int iteration = 0;
+    while (zx * zx + zy * zy < 4.0 && iteration < MAX_ITERATIONS)
+    {
+        float xtemp = zx * zx - zy * zy + cx;
+        zy = 2.0 * zx * zy + cy;
+        zx = xtemp;
+        iteration++;
+    }
+
+    Color color = getColor(iteration);
+    image[(y * WIDTH + x) * 3 + 0] = color.red;
+    image[(y * WIDTH + x) * 3 + 1] = color.green;
+    image[(y * WIDTH + x) * 3 + 2] = color.blue;
+}
+
+int main()
+{
+    constexpr dim3 block(16, 16);
+    constexpr dim3 grid((WIDTH + block.x - 1) / block.x, (HEIGHT + block.y - 1) / block.y);
+
+    uint8_t* deviceImage;
+    cudaMalloc(&deviceImage, WIDTH * HEIGHT * 3 * sizeof(uint8_t));
+    uint8_t* hostImage = new uint8_t[WIDTH * HEIGHT * 3];
+    char filename[256];
+
+    float centerX = -1.4002;  // X-coordinate of the center of the zoom
+    float centerY = 0.0;   // Y-coordinate of the center of the zoom
+
+    for (int frame = 0; frame < FRAMES; frame++)
+    {
+        float zoom = std::sqrt(frame + 1) * ZOOM_FACTOR;
+        generateMandelbrot<<<grid, block>>>(deviceImage, zoom, centerX, centerY);
+        cudaDeviceSynchronize();
+
+        cudaMemcpy(hostImage, deviceImage, WIDTH * HEIGHT * 3 * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+        snprintf(filename, sizeof(filename), "./frames/frame%d.png", frame);
+        stbi_write_png(filename, WIDTH, HEIGHT, 3, hostImage, WIDTH * 3);
+    }
+
+    delete[] hostImage;
+    cudaFree(deviceImage);
+
+    return 0;
+}
